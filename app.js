3387dd053f70e9c745de54def2465ad335ed6d54
Fanisi Oluwatimilehin <104774643+DevPhancey@users.noreply.github.com>
2023-09-08 17:08:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
  function updateDateTime() {
             const currentDate = new Date();
-            const currentUTC = currentDate.toUTCString();
+            const currentUTC = currentDate.getUTCHours();
+            const minuteUTC = currentDate.getUTCMinutes();
+            const seconedUTC = currentDate.getUTCMilliseconds();
             const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
             const currentDayIndex = currentDate.getUTCDay();
             const currentDayOfTheWeek = daysOfWeek[currentDayIndex];
@@ -8,6 +10,8 @@
             // Update the content of elements with specific IDs
             document.getElementById('currenttime').textContent = currentUTC;
             document.getElementById('currentday').textContent = currentDayOfTheWeek;
+            document.getElementById('seconedUTC').textContent = seconedUTC;
+            document.getElementById('minuteUTC').textContent = minuteUTC;
         }
 
         // Call the function to update the elements when the page loads
